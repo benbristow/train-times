7cc47b4801e153767aeed23148b8b18fc6480506
Ben Bristow <me@benbristow.co.uk>
2018-02-23 01:29:43 +0000

    Remove question mark from help text

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ require_all 'lib'
 Clamp do
   parameter 'ORIGIN/ARRIVAL', 'Departures: Starting station / Arrivals: Station arriving at', attribute_name: :primary_crs
   parameter '[DESTINATION/FROM]', 'Departures: Destination station / Arrivals: Station train arriving from', attribute_name: :secondary_crs
-  option ['-a', '--arrivals'], :flag, 'Show arrivals instead of departures?'
+  option ['-a', '--arrivals'], :flag, 'Show arrivals instead of departures'
 
   def execute
     @primary_station = Station.new(primary_crs)
