e000b87e234c1444c952b8129b845c1f8e4d01fc
Ben Bristow <me@benbristow.co.uk>
2018-04-02 16:21:18 +0100

    Remove code duplication

--- main.rb
+++ main.rb
@@ -16,7 +16,9 @@ Clamp do
     parameter '[From]', 'Station trains are arriving from', attribute_name: :from
 
     def execute
-      puts ServiceTable.new(station_board(arriving, from, 'arrivals')).to_s
+      with_error_handling do
+        puts ServiceTable.new(station_board(arriving, from, 'arrivals')).to_s
+      end
     end
   end
 
@@ -25,7 +27,9 @@ Clamp do
     parameter '[To]', 'Station trains are travelling to', attribute_name: :to
 
     def execute
-      puts ServiceTable.new(station_board(departing, to, 'departures')).to_s
+      with_error_handling do
+        puts ServiceTable.new(station_board(departing, to, 'departures')).to_s
+      end
     end
   end
 
@@ -33,33 +37,31 @@ Clamp do
     parameter 'Service ID', 'Service ID of the train service', attribute_name: :service_id
 
     def execute
-      service = Service.from_service_id(service_id)
-      puts CallingPointTable.new(service.timetable.calling_points).to_s
-    rescue TrainTimesError => error
-      puts "Error: #{error.message}"
-    rescue Savon::SOAPFault
-      puts 'Error: Server error - You probably put in an invalid service ID'
-    rescue SocketError
-      puts 'Unable to connect to National Rail API. Perhaps there is an issue with your internet connection?'
-    rescue Exception => error
-      puts "Unknown error: #{error}"
+      with_error_handling do
+        service = Service.from_service_id(service_id)
+        puts CallingPointTable.new(service.timetable.calling_points).to_s
+      end
     end
   end
 
   private
+  
+  def with_error_handling
+    yield
+  rescue TrainTimesError => error
+    puts "Error: #{error.message}"
+  rescue Savon::SOAPFault
+    puts 'Error: Request error. You probably inputted something wrong'
+  rescue SocketError
+    puts 'Unable to connect to National Rail API. Perhaps there is an issue with your internet connection?'
+  rescue Exception => error
+    puts "Unknown error: #{error}"
+  end
 
   def station_board(primary_crs, secondary_crs, mode)
     @primary_station = Station.new(primary_crs)
 
     return @primary_station.arrivals(from: (secondary_crs || '').upcase) if mode == 'arrivals'
     return @primary_station.departures(to: (secondary_crs || '').upcase) if mode == 'departures'
-  rescue TrainTimesError => error
-    puts "Error: #{error.message}"
-  rescue Savon::SOAPFault
-    puts 'Error: Server error - You probably put in an invalid station code'
-  rescue SocketError
-    puts 'Unable to connect to National Rail API. Perhaps there is an issue with your internet connection?'
-  rescue Exception => error
-    puts "Unknown error: #{error}"
   end
 end
